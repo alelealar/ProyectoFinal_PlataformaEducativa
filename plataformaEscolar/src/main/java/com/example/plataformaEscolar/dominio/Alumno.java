4518b264ac5f77e75c72b2cc838e67ea5ffd4027
jruben28 <140290443+jruben28@users.noreply.github.com>
2026-09-20 15:55:08 -0700

    Create Alumno.java

---
+++ plataformaEscolar/src/main/java/com/example/plataformaEscolar/dominio/Alumno.java
@@ -0,0 +1,72 @@
+package com.example.plataformaEscolar.dominio;
+
+public class Alumno {
+
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String correo;
+    private String matricula;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellidoPaterno, String apellidoMaterno, String correo, String matricula) {
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.correo = correo;
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumno{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidoPaterno='" + apellidoPaterno + '\'' +
+                ", apellidoMaterno='" + apellidoMaterno + '\'' +
+                ", correo='" + correo + '\'' +
+                ", matricula='" + matricula + '\'' +
+                '}';
+    }
+}
